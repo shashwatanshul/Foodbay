edd72a532407e9a1f63ddea51cf6f4f77e71634a
shashwatanshul <shashwatanshul@gmail.com>
2024-02-10 22:21:13 +0530

    fourth commit
    fourth commmit

--- server.js
+++ server.js
@@ -39,11 +39,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 //++++++++++++++++++++++++++++++++++++++++++
-app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 //--------------------------------------------------------------------------------
 
 app.use(express.json());
