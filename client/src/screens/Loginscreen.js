acbe87ce73ca762f4885b4476b3ec38e5a099ce0
shashwatanshul <shashwatanshul@gmail.com>
2024-02-21 18:58:12 +0530

    eighteenth commit
    eighteenth commit

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -58,7 +58,7 @@ export default function Loginscreen() {
             </button>
             <br></br>
             <a style={{ color: "black" }} href="/register">
-              Click Here To Register
+              Click Here To Register if You are New User
             </a>
           </div>
         </div>
