7d07af120aaa967ad79a166bd2fe586ddf1a0a69
shashwatanshul <shashwatanshul@gmail.com>
2024-02-21 19:16:39 +0530

    21st commit
    21st commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "../actions/userActions";
+
+function Navbar() {
+  const cartstate = useSelector((state) => state.cartReducer);
+  const userstate = useSelector((state) => state.loginUserReducer);
+  const { currentUser } = userstate;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+        <a className="navbar-brand" href="/">
+          MENU
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            {currentUser ? (
+              <div className="dropdown">
+                <a
+                  className="dropdown-toggle nav-link"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {currentUser.name}
+                </a>
+
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="/orders">
+                      Orders
+                    </a>
+                  </li>
+                  {currentUser.isAdmin ? (
+                    <li>
+                      <a className="dropdown-item" href="/admin">
+                        Admin
+                      </a>
+                    </li>
+                  ) : (
+                    <li></li>
+                  )}
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      onClick={() => {
+                        dispatch(logoutUser());
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <li className="nav-item">
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+            )}
+            {currentUser ? (
+              <li className="nav-item">
+                <a className="nav-link" href="/cart">
+                  Cart {cartstate.cartItems.length}
+                </a>
+              </li>
+            ) : (
+              <li></li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
